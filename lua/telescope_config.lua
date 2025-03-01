2368c456e587260bf2fb166632334e889daa4883
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-02-28 21:06:25 -0500

    fix telescope

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -1,47 +1,40 @@
--- require('telescope').load_extension('fzf')
--- require('telescope').setup {
---         extensions = {
---                 fzf = {
---                         fuzzy = true,                   -- false will only do exact matching
---                         override_generic_sorter = true, -- override the generic sorter
---                         override_file_sorter = true,    -- override the file sorter
---                         case_mode = "smart_case",       -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
---                 },
---         },
---         defaults = {
---                 -- ignore_symbols = {"field", "struct"},
---                 file_ignore_patterns = { "vendor", "go.sum", "go.mod", "module", ".git", "gen", "*.pb.go", "mod", "Cellar", "mocks" },
---                 mappings = {
---                         i = {
---                                 ["<C-n>"] = false,
---                                 ["<C-p>"] = false,
---                                 ["<esc>"] = require('telescope.actions').close,
---                                 ["<C-[>"] = require('telescope.actions').close,
---                                 ["<C-j>"] = require('telescope.actions').move_selection_next,
---                                 ["<C-k>"] = require('telescope.actions').move_selection_previous,
---                                 ["<leader>q"] = require('telescope.actions').send_selected_to_qflist + require('telescope.actions').open_qflist,
---                         },
---                 }
---         }
--- }
--- lua/telescope_config.lua
 local telescope = require('telescope')
+local actions = require('telescope.actions')
 
-telescope.setup({
-        defaults = {
-                file_ignore_patterns = { "vendor", ".git", "node_modules", "*.pb.go", "gen", "mocks" },
-                layout_config = {
-                        horizontal = { width = 0.9, preview_cutoff = 120 },
-                },
-        },
+telescope.setup {
         extensions = {
                 fzf = {
-                        fuzzy = true,
-                        override_generic_sorter = true,
-                        override_file_sorter = true,
-                        case_mode = "smart_case",
+                        fuzzy = true,                   -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true,    -- override the file sorter
+                        case_mode = "smart_case",       -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
                 },
+        },
+        defaults = {
+                vimgrep_arguments = {
+                        'rg',
+                        '--color=never',
+                        '--no-heading',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '--smart-case'
+                },
+                -- ignore_symbols = {"field", "struct"},
+                file_ignore_patterns = { "vendor", "go.sum", "go.mod", "module", ".git", "gen", "*.pb.go", "mod", "Cellar", "mocks" },
+                mappings = {
+                        i = {
+                                ["<C-n>"] = false,
+                                ["<C-p>"] = false,
+                                ["<esc>"] = actions.close,
+                                ["<C-[>"] = actions.close,
+                                ["<C-j>"] = actions.move_selection_next,
+                                ["<C-k>"] = actions.move_selection_previous,
+                                ["<leader>q"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        },
+                }
         }
-})
+}
 
+-- Load telescope extensions
 telescope.load_extension('fzf')
