7ab9e3189f8f19964dd3ad0facbd12145c59a6c7
nicolas-martin <nicolas.martin.0986@gmail.com>
2025-03-23 19:41:26 -0400

    --wip--

--- init.lua
+++ init.lua
@@ -1,8 +1,26 @@
-require('settings')
-require('keymaps')
-require('plugins')
-require('lsp')
-require('autocmds')
-require('telescope_config')
-require('cmp_config')
-require('treesitter')
+-- init.lua
+-- Ensure lazy.nvim is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins
+require("lazy").setup("plugins")
+
+-- Load additional configurations
+require("settings")
+require("keymaps")
+require("lsp")
+require("autocmds")
+require("telescope_config")
+require("cmp_config")
+require("treesitter")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,74 +1,114 @@
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'preservim/nerdtree'
-	use 'tpope/vim-repeat'
-	use 'tpope/vim-surround'
-	use 'tpope/vim-commentary'
-	-- use 'ellisonleao/gruvbox.nvim'
-	use 'EdenEast/nightfox.nvim'
-	use 'fatih/vim-go' -- vim-go doesn't need Lua setup
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+-- lua/plugins.lua
+return {
+	-- Package manager
+	{ "folke/lazy.nvim",       version = "*" },
 
-	-- Collection of common configurations for the Nvim LSP client
-	use 'neovim/nvim-lspconfig'
+	-- File explorer
+	{ "preservim/nerdtree" },
 
-	-- LSP completion source for nvim-cmp
-	use {
+	-- Repeat plugin
+	{ "tpope/vim-repeat" },
+
+	-- Surround plugin
+	{ "tpope/vim-surround" },
+
+	-- Commenting utility
+	{ "tpope/vim-commentary" },
+
+	-- Colorschemes
+	{ "EdenEast/nightfox.nvim" },
+
+	-- Go development plugin
+	{ "fatih/vim-go",          build = ":GoUpdateBinaries" },
+
+	-- Treesitter configurations and abstraction layer
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				-- Add other configurations here
+			})
+		end,
+	},
+
+	-- LSP configurations
+	{ "neovim/nvim-lspconfig" },
+
+	-- LSP signature hint when you type
+	{
 		"ray-x/lsp_signature.nvim",
 		config = function()
 			require("lsp_signature").setup()
-		end
-	}
-	-- add the nice source + completion item kind to the menu
-	use {
-		'onsails/lspkind.nvim',
-		-- config = function()
-		-- 	require('lspkind').init()
-		-- end
-	}
+		end,
+	},
+
+	-- VSCode-like pictograms for neovim lsp completion items
+	{ "onsails/lspkind.nvim" },
 
 	-- Autocompletion framework
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-buffer")
-
-	-- Snippets
-	use("L3MON4D3/LuaSnip")
-	use("rafamadriz/friendly-snippets")
-
-	-- use 'hrsh7th/cmp-cmdline'
-
-	-- nice icons
-	use 'kyazdani42/nvim-web-devicons'
-
-	-- Fuzzy finder
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
-	-- Copilot setup
-	use {
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-buffer" },
+			-- Snippet engine and sources
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
+		},
 		config = function()
-			require("copilot").setup({
-				suggestion = { enabled = false },
-				panel = { enabled = false },
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "buffer" },
+					{ name = "luasnip" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
 			})
 		end,
-	}
-	use {
-		"zbirenbaum/copilot-cmp",
-		after = { "copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup()
-		end
-	}
+	},
 
-	use {
-		"pmizio/typescript-tools.nvim",
-		requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" }
-	}
-end)
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		cmd = "Telescope",
+		config = function()
+			require("telescope").setup()
+		end,
+	},
+
+	-- Git integration
+	{ "tpope/vim-fugitive" },
+
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = { theme = "auto" },
+				-- Add other configurations here
+			})
+		end,
+	},
+
+	-- Additional plugins can be added here
+}
