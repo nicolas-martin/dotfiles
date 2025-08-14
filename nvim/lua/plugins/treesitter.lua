c218caf0baa60955029ba59da4898ba4b27f3fed
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-08-14 12:45:14 -0400

    --wip--

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -45,19 +45,19 @@ return {
 					set_jumps = true,
 					goto_next_start = {
 						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
+						-- ["]c"] = "@class.outer",
 					},
 					goto_next_end = {
 						["]F"] = "@function.outer",
-						["]C"] = "@class.outer",
+						-- ["]C"] = "@class.outer",
 					},
 					goto_previous_start = {
 						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
+						-- ["[c"] = "@class.outer",
 					},
 					goto_previous_end = {
 						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
+						-- ["[C"] = "@class.outer",
 					},
 				},
 			},
